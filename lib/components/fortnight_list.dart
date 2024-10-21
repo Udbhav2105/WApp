df2a481a1fd1df433e2ba4e33aaa3789c36ce9b2
Udbhav <udbhav.k1234@gmail.com>
2024-10-21 22:09:52 +0530

    15 forcast page added

--- lib/components/fortnight_list.dart
+++ lib/components/fortnight_list.dart
@@ -1,40 +1,62 @@
 import 'package:flutter/material.dart';
 
 class FortnightList extends StatelessWidget {
-  final iconUrl;
-  final date;
+  final String iconUrl;
+  final String date;
+  final String weekday;
   final temp;
-  const FortnightList({super.key, this.iconUrl, this.temp, this.date});
+
+  const FortnightList(
+      {super.key,
+      required this.iconUrl,
+      this.temp,
+      required this.date,
+      day,
+      required this.weekday});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(2,0,2,5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.network(iconUrl,height: 80,),
-          Column(
-            children: [
-              Text('Date: ${date}',style:
-                TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
-              Text('Temperature: ${temp.toString()}°C',style:
-                TextStyle(
-                  fontSize: 15,
-                ),),
-            ],
-          )
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.blue.withOpacity(0.1),
-        borderRadius: BorderRadius.all(
+        margin: const EdgeInsets.fromLTRB(2, 0, 2, 5),
+        decoration: BoxDecoration(
+            // color: Colors.blue.withOpacity(0.1),
+            borderRadius: const BorderRadius.all(
           Radius.circular(10),
-        )
-      ),
-    );
+        )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    weekday,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black54),
+                  ),
+                  Text(
+                    date,
+                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                  ),
+                ],
+              ),
+              Text(
+                temp.toString() + '°C',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Image.network(
+                iconUrl,
+                height: 80,
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/pages/search%20page.dart';
 import 'package:weather_app/pages/weather_page.dart';
-
+import 'package:weather_app/pages/fortnight_forecast.dart';
 void main() => runApp(MaterialApp(
+
       initialRoute: '/',
       routes: {
         '/': (context) => const SearchLoc(),
         '/weatherPage': (context) =>  WeatherPage(),
+        '/15days':(context) => FortnightForecast(),
       },
     ));
